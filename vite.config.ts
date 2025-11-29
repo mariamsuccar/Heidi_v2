0a82fb38add38b846e2ad25bfbd08b67549eb67b
mariamsuccar <msuccar43@gmail.com>
2025-11-29 13:31:25 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig({
+  plugins: [react()],
+  root: "src",
+  build: {
+    outDir: "../dist",
+  }
+});
 
   return {
     plugins: [react()],
